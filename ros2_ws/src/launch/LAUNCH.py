7a10131dfba9609a952252b15a1b47bb9822d03c
VBS2004 <66900514+VBS2004@users.noreply.github.com>
2024-01-07 17:11:35 +0530

    Update LAUNCH.py

--- ros2_ws/src/launch/LAUNCH.py
+++ ros2_ws/src/launch/LAUNCH.py
@@ -5,10 +5,10 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='my_robot_motion_planning',
-            executable='/root/ros2_ws/build/my_robot_motion_planning/my_motion_planner',
+            executable='/root/Ompl_PlannerTurtlebot3/ros2_ws/build/my_robot_motion_planning/my_motion_planner',
             name='my_motion_planner',
             output='screen',
-            parameters=[{'map_file': '/root/ros2_ws/src/maps/your_map.yaml'}]  # Set the default value here
+            parameters=[{'map_file': '/root/Ompl_PlannerTurtlebot3/ros2_ws/src/maps/your_map.yaml'}]  # Set the default value here
         )
     ])
 
